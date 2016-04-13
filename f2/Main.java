f2b3075bb30029f07732bf6774d23c00d3766f97
Somchai Wichaidit <pigsoft.g@gmail.com>
2016-04-13 07:05:35 +0700

    #01 สร้างหน้าต่างเปล่าๆ เรียบร้อยแล้วครับ

---
+++ f2/Main.java
@@ -0,0 +1,14 @@
+package f2;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("Space War");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 650);
+		
+		frame.setVisible(true);
+	}
+}
